80cf7ed0586a53a4b80093525628d688c76fea01
Gonzalez Carlos <goniti@hotmail.fr>
2019-07-11 18:18:08 +0200

    init todo array

--- my-todolist/src/components/TodoStore.ts
+++ my-todolist/src/components/TodoStore.ts
@@ -1,36 +1,38 @@
-import { Todo } from './Interfaces';
+import { Todo } from "./Interfaces";
 import uuidv4 from "uuidv4";
 
 export default class TodoStore {
+  public todos: Todo[] = [];
 
-    public todos: Todo[]
+  addTodo ( title: string ): void {
+    this.todos = [
+      {
+        id: uuidv4(),
+        title: title,
+        completed: false
+      },
+      ...this.todos
+    ];
+  }
 
-    addTodo ( title: string ): void {
-        this.todos = [{
-            id: uuidv4(),
-            title: title,
-            completed: false
-        }, ...this.todos]
-    }
+  removeTodo ( todo: Todo ): void {
+    this.todos = this.todos.filter( t => t !== todo );
+  }
 
-    removeTodo ( todo: Todo ): void {
-        this.todos = this.todos.filter( t => t !== todo )
-    }
+  toggleTodo ( todo: Todo, completed = true ): void {
+    this.todos = this.todos.map( t => ( t !== todo ? { ...t, completed } : t ) );
+  }
 
-    toggleTodo ( todo: Todo, completed = true ): void {
-        this.todos = this.todos.map( t => t !== todo ? { ...t, completed } : t )
-    }
+  toggleAll ( completed = true ) {
+    this.todos = this.todos.map( t =>
+      completed !== t.completed ? { ...t, completed } : t
+    );
+  }
+  updateTitle ( todo: Todo, title: string ): void {
+    this.todos = this.todos.map( t => ( t !== todo ? { ...t, title } : t ) );
+  }
 
-    toggleAll ( completed = true ) {
-        this.todos = this.todos.map( t => completed !== t.completed ? { ...t, completed } : t )
-    }
-    updateTitle ( todo: Todo, title: string ): void {
-        this.todos = this.todos.map( t => t !== todo ? { ...t, title } : t )
-
-    }
-
-    cleanCompleted (): void {
-        this.todos = this.todos.filter( t => !t.completed )
-    }
-
+  cleanCompleted (): void {
+    this.todos = this.todos.filter( t => !t.completed );
+  }
 }
